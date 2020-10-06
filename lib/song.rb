139c2e278d790c1945b40007bd13b81f17a73062
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-10-06 23:24:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,11 +13,11 @@ class Song
   end
   
   def artist_name
-    if self.artist.name == self.artist.name
-      self.artist.name 
-    else
+    if self.artist.name == ""
       puts "nil"
-  end
+    else
+      self.artist.name
+    end
   end
   
 end
