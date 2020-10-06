d6a98aa76ade1bc0eb2c5639922068e04fb722b9
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-10-06 22:48:37 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,38 @@
+class Author
+  attr_accessor :name, :title, :post
+ 
+  def initialize(name)
+    @name = name
+  end
+
+  def title
+    @title = title
+  end
+  
+  def author_name
+    @name = name
+  end
+
+  def add_post(name)
+    name.author = self
+  end
+ 
+  def posts
+    Post.all.select do |title| title.author == self
+    end
+  end
+  
+  def add_post_by_title(title)
+    new_post = Post.new(title)
+    new_post.author = self
+  end
+  
+  def self.post_count
+    Post.all.count
+  end
+  
+  def author_name
+  
+  end
+  
+end
